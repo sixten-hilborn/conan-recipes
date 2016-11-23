d493446617b8aefde9ca9a5be2886ed70f3248a2
a_teammate <madoe3@web.de>
2016-11-23 01:35:03 +0100

    fix some raw stuff

--- conanfile.py
+++ conanfile.py
@@ -6,17 +6,17 @@ from conans import CMake, ConfigureEnvironment
 from conans.model.settings import Settings
 import copy
 
-class SDLConan(ConanFile):
+class SDL_MixerConan(ConanFile):
     name = "SDL2_mixer"
     version = "2.0.1"
     folder = "SDL2_mixer-%s" % version
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False]
-            #, "fPIC": [True, False]
+            , "fPIC": [True, False]
             }
-    default_options = '''shared=False'''
+    default_options = '''shared=False
+    fPIC=True'''
     generators = "cmake"
- #   url="https://bitbucket.org/mutcoll/conan-sdl2-mixer" # TODO change
     requires = "SDL2/2.0.5@lasote/stable"
     license="MIT"
 
@@ -44,79 +44,37 @@ class SDLConan(ConanFile):
     def build_with_make(self):
         
         env = ConfigureEnvironment(self.deps_cpp_info, self.settings)
-        #if self.options.fPIC:
-            #env_line = env.command_line.replace('CFLAGS="', 'CFLAGS="-fPIC ')
-        #else:
-            #env_line = env.command_line
+        if self.options.fPIC:
+            env_line = env.command_line.replace('CFLAGS="', 'CFLAGS="-fPIC ')
+        else:
+            env_line = env.command_line
         env_line = env.command_line.replace('LDFLAGS="', 'LDFLAGS="-Wl,--no-as-needed ')
             
-        custom_vars = 'LIBPNG_LIBS= SDL_LIBS= LIBPNG_CFLAGS='
+        custom_vars = '' # Original: 'LIBPNG_LIBS= SDL_LIBS= LIBPNG_CFLAGS=' TODO SDL_VERSION
         sdl2_config_path = os.path.join(self.deps_cpp_info["SDL2"].lib_paths[0], "sdl2-config")
          
         self.run("cd %s" % self.folder)
         self.run("chmod a+x %s/configure" % self.folder)
         self.run("chmod a+x %s" % sdl2_config_path)
         
+        configure_args = ["--disable-music-cmd", "--disable-music-mod-modplug", "--disable_music_mod_mikmod", "--disable-music-midi", # Stuff we give a shit about
+                          "--disable-music-ogg", "--disable-music-flac", "--disable-music-mp3", # Stuff we declare broken atm
+                          "--disable_sdltest", "--disable_smpegtest", ""] # We disable all manual tests
         self.output.warn(env_line)
         if self.settings.os == "Macos": # Fix rpath, we want empty rpaths, just pointing to lib file
             old_str = "-install_name \$rpath/"
             new_str = "-install_name "
             replace_in_file("%s/configure" % self.folder, old_str, new_str)
         
-        #old_str = '#define LOAD_PNG_DYNAMIC "$png_lib"'
-        #new_str = ''
-        #replace_in_file("%s/configure" % self.folder, old_str, new_str)
-        
-        #old_str = '#define LOAD_JPG_DYNAMIC "$jpg_lib"'
-        #new_str = ''
-        #replace_in_file("%s/configure" % self.folder, old_str, new_str)
-        
-        configure_command = 'cd %s && %s SDL2_CONFIG=%s %s ./configure' % (self.folder, env_line, sdl2_config_path, custom_vars)
+        configure_command = 'cd %s && %s SDL2_CONFIG=%s %s ./configure %s' % (self.folder, env_line, sdl2_config_path, custom_vars, " ".join(configure_args))
         self.output.warn("Configure with: %s" % configure_command)
         self.run(configure_command)
-        
-        #old_str = 'DEFS = '
-        #new_str = 'DEFS = -DLOAD_JPG=1 -DLOAD_PNG=1 ' # Trust conaaaan!
-        #replace_in_file("%s/Makefile" % self.folder, old_str, new_str)
-        
-        #old_str = '\nLIBS = '
-        #new_str = '\n# Removed by conan: LIBS2 = '
-        #replace_in_file("%s/Makefile" % self.folder, old_str, new_str)
-        
-        #old_str = '\nLIBTOOL = '
-        #new_str = '\nLIBS = %s \nLIBTOOL = ' % " ".join(["-l%s" % lib for lib in self.deps_cpp_info.libs]) # Trust conaaaan!
-        #replace_in_file("%s/Makefile" % self.folder, old_str, new_str)
-        #
-        #old_str = '\nLIBPNG_CFLAGS ='
-        #new_str = '\n# Commented by conan: LIBPNG_CFLAGS ='
-        #replace_in_file("%s/Makefile" % self.folder, old_str, new_str)
-        #
-        #old_str = '\nLIBPNG_LIBS ='
-        #new_str = '\n# Commented by conan: LIBPNG_LIBS ='
-        #replace_in_file("%s/Makefile" % self.folder, old_str, new_str)
-        #
-        #old_str = '\nOBJCFLAGS'
-        #new_str = '\n# Commented by conan: OBJCFLAGS ='
-        #replace_in_file("%s/Makefile" % self.folder, old_str, new_str)
-        #
-        #old_str = '\nSDL_CFLAGS ='
-        #new_str = '\n# Commented by conan: SDL_CFLAGS ='
-        #replace_in_file("%s/Makefile" % self.folder, old_str, new_str)
-        #
-        #old_str = '\nSDL_LIBS ='
-        #new_str = '\n# Commented by conan: SDL_LIBS ='
-        #replace_in_file("%s/Makefile" % self.folder, old_str, new_str)
-        #
-        #old_str = '\nCFLAGS ='
-        #new_str = '\n# Commented by conan: CFLAGS ='
-        #replace_in_file("%s/Makefile" % self.folder, old_str, new_str)
-        #
+
         old_str = '\n# Commented by conan: CFLAGS ='
-        #fpic = "-fPIC"  if self.options.fPIC else ""
-        #m32 = "-m32" if self.settings.arch == "x86" else ""
+        fpic = "-fPIC"  if self.options.fPIC else ""
+        m32 = "-m32" if self.settings.arch == "x86" else ""
         debug = "-g" if self.settings.build_type == "Debug" else "-s -DNDEBUG"
-        #new_str = '\nCFLAGS =%s %s %s %s\n# Commented by conan: CFLAGS =' % (" ".join(self.deps_cpp_info.cflags), fpic, m32, debug)
-        new_str = '\nCFLAGS =%s %s\n# Commented by conan: CFLAGS =' % (" ".join(self.deps_cpp_info.cflags), debug)
+        new_str = '\nCFLAGS =%s %s %s %s\n# Commented by conan: CFLAGS =' % (" ".join(self.deps_cpp_info.cflags), fpic, m32, debug)
         replace_in_file("%s/Makefile" % self.folder, old_str, new_str)
         
         self.run("cd %s && %s make" % (self.folder, env_line))
@@ -131,10 +89,10 @@ class SDLConan(ConanFile):
         if self.settings.os == "Windows":
             if self.settings.arch == "x86":
                 self.copy(pattern="*.lib", dst="lib", src="%s/lib/x86" % self.folder, keep_path=False)
-                self.copy(pattern="*.dll*", dst="lib", src="%s/lib/x86" % self.folder, keep_path=False)
+                self.copy(pattern="*.dll*", dst="bin", src="%s/lib/x86" % self.folder, keep_path=False)
             else:
                 self.copy(pattern="*.lib", dst="lib", src="%s/lib/x64" % self.folder, keep_path=False)
-                self.copy(pattern="*.dll*", dst="lib", src="%s/lib/x64" % self.folder, keep_path=False)
+                self.copy(pattern="*.dll*", dst="bin", src="%s/lib/x64" % self.folder, keep_path=False)
         if not self.options.shared:
             self.copy(pattern="*.a", dst="lib", src="%s" % self.folder, keep_path=False)   
         else:
