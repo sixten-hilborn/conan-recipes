19c61f92ee1f8a7fa6a8e80463e65353ab332f61
Sixten Hilborn <hilborn90@gmail.com>
2017-01-21 14:55:43 +0100

    Download tgz for linux instead of package managers

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,5 @@
 from conans import ConanFile
-from conans.tools import download, SystemPackageTool
+from conans.tools import get, download, SystemPackageTool
 import os
 
 class CgConan(ConanFile):
@@ -10,27 +10,40 @@ class CgConan(ConanFile):
     settings = "os", "arch"
     url = "http://github.com/sixten-hilborn/conan-cg"
     license = "https://bitbucket.org/cabalistic/ogredeps/src/bfc878e4fd9a3e026de73114cf42abe2787461b8/src/Cg/license.txt"
+    installed = False
 
     def system_requirements(self):
+        # TODO: Decide if this should be used or just download binaries manually in source()
         if self.settings.os == "Linux":
             installer = SystemPackageTool()
             installer.update()
-            installer.install("nvidia-cg-toolkit")
+            retcode = installer.install("nvidia-cg-toolkit")
+            self.installed = (retcode == 0)
 
     def source(self):
         if self.settings.os == "Linux":
-            pass
+            if not self.installed:
+                self.source_linux()
         elif self.settings.os == "Windows":
-            def get_cg(path):
-                filename = os.path.basename(path)
-                download("https://bitbucket.org/cabalistic/ogredeps/raw/bfc878e4fd9a/src/Cg/{0}".format(path), filename)
-            get_cg('include/Cg/cg.h')
-            if self.settings.arch == 'x86':
-                get_cg('bin/cg.dll')
-                get_cg('lib/cg.lib')
-            else:
-                get_cg('bin64/cg.dll')
-                get_cg('lib64/cg.lib')
+            self.source_windows()
+
+    def source_linux(self):
+        if self.settings.arch == 'x86':
+            get("http://developer.download.nvidia.com/cg/Cg_3.1/Cg-3.1_April2012_x86.tgz")
+        elif self.settings.arch == 'x86_64':
+            get("http://developer.download.nvidia.com/cg/Cg_3.1/Cg-3.1_April2012_x86_64.tgz")
+
+    def source_windows(self):
+        def get_cg(path):
+            filename = os.path.basename(path)
+            download("https://bitbucket.org/cabalistic/ogredeps/raw/bfc878e4fd9a/src/Cg/{0}".format(path), filename)
+        get_cg('include/Cg/cg.h')
+        if self.settings.arch == 'x86':
+            get_cg('bin/cg.dll')
+            get_cg('lib/cg.lib')
+        else:
+            get_cg('bin64/cg.dll')
+            get_cg('lib64/cg.lib')
 
     def build(self):
         pass
@@ -39,7 +52,7 @@ class CgConan(ConanFile):
         self.copy(pattern="*.h", dst="include/Cg", keep_path=False)
         self.copy("*.lib", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.so*", dst="lib", keep_path=False, links=True)
         self.copy("*.dll", dst="bin", keep_path=False)
 
     def package_info(self):
