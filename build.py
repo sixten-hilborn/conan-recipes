6ff4435eee54e45f653a91d7d932bce50d2b3ceb
Sixten Hilborn <hilborn90@gmail.com>
2017-03-01 19:16:43 +0100

    Re-enable linux 32 bit

--- build.py
+++ build.py
@@ -4,6 +4,5 @@ import platform
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(pure_c=True)
-    builder.builds = [[settings, options] for settings, options in builder.builds if not (platform.system() == "Linux" and settings["arch"] == "x86")]
     builder.run()
 
