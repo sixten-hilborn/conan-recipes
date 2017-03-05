b48f1464993e337f581ad89851de951fe14d598f
Sixten Hilborn <hilborn90@gmail.com>
2017-03-05 17:11:14 +0100

    Add dll_with_static_runtime=True for CI

--- build.py
+++ build.py
@@ -3,6 +3,6 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds(shared_option_name="lua:shared", pure_c=True)
+    builder.add_common_builds(shared_option_name="lua:shared", pure_c=True, dll_with_static_runtime=True)
     builder.run()
 
