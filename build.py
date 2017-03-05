cd1428b18ea0e9c66af3880fb0148cad3ef9534b
Sixten Hilborn <hilborn90@gmail.com>
2017-03-05 09:26:30 +0100

    Update build.py

--- build.py
+++ build.py
@@ -2,7 +2,13 @@ from conan.packager import ConanMultiPackager
 import platform
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
+    builder = ConanMultiPackager(args="--build missing")
     builder.add_common_builds(shared_option_name="CEGUI:shared", pure_c=False)
+    # Disable VS2010 because of missing DirectX stuff
+    builder.builds = [
+        [settings, options]
+        for settings, options in builder.builds
+        if not (settings["compiler"] == "Visual Studio" and settings["compiler.version"] == "10")
+    ]
     builder.run()
 
