5b97fd744161ec0fa44dc064d20b5e3cab7b171d
Sixten Hilborn <hilborn90@gmail.com>
2017-03-05 13:37:08 +0100

    Use new cmake helper and add -lpthread for linux

--- conanfile.py
+++ conanfile.py
@@ -28,15 +28,16 @@ class OdeConan(ConanFile):
 
     def build(self):
         cmake = CMake(self.settings)
-        cd_build = 'cd ' + self.folder
-        options = (
-            '-DBUILD_SHARED_LIBS={0} '
-            '-DUSE_DOUBLE_PRECISION={1} ').format(
-                1 if self.options.shared else 0,
-                1 if self.options.double_precision else 0)
-        build_options = '-- -j{0}'.format(cpu_count()) if self.settings.compiler == 'gcc' else ''
-        self.run_and_print('%s && cmake . %s %s' % (cd_build, cmake.command_line, options))
-        self.run_and_print("%s && cmake --build . %s %s" % (cd_build, cmake.build_config, build_options))
+        options = {
+            'BUILD_SHARED_LIBS': self.options.shared,
+            'USE_DOUBLE_PRECISION': self.options.double_precision
+        }
+        cmake.configure(self, build_dir=self.folder, source_dir='.', defs=options)
+
+        build_args = ['--']
+        if self.settings.compiler == 'gcc':
+            build_args.append('-j{0}'.format(cpu_count()))
+        cmake.build(self, args=build_args)
 
     def package(self):
         bin_dir = "{0}/bin".format(self.folder)
@@ -59,6 +60,5 @@ class OdeConan(ConanFile):
             name += 'd'
         self.cpp_info.libs = [name]
 
-    def run_and_print(self, command):
-        self.output.warn(command)
-        self.run(command)
+        if self.settings.os == 'Linux':
+            self.cpp_info.libs.append('pthread')
