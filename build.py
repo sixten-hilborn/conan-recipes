255c5839ac9a6e3a071d4a13bacc4d27d233e142
Sixten Hilborn <hilborn90@gmail.com>
2017-05-25 00:34:44 +0200

    Update build.py to fix macOS

--- build.py
+++ build.py
@@ -5,11 +5,15 @@ import platform
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name="CEGUI:shared", pure_c=False)
+
+    extra_settings = []
+    if os_info.is_macos:
+        extra_settings += ["CEGUI:with_ois=False", "CEGUI:with_sdl=True"]  # Build with SDL since OIS doesn't work on macOS
     # Disable VS2010 because of missing DirectX stuff
     # Disable x86 Linux builds (OGRE x86 package does not work at the moment)
     builder.builds = [
-        [settings, options]
-        for settings, options in builder.builds
+        [settings + extra_settings, options, env_vars, build_requires]
+        for settings, options, env_vars, build_requires in builder.builds
         if not (settings["compiler"] == "Visual Studio" and settings["compiler.version"] == "10")
         and not (os_info.is_linux and settings["arch"] == "x86")
     ]
