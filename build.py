7904e4030f8c34dd34a81b7d53c4a67fc680e13f
Sixten Hilborn <hilborn90@gmail.com>
2017-05-25 13:21:07 +0200

    Fix macOS options

--- build.py
+++ build.py
@@ -6,13 +6,13 @@ if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name="CEGUI:shared", pure_c=False)
 
-    extra_settings = []
+    extra_options = []
     if os_info.is_macos:
-        extra_settings += [("CEGUI:with_ois", "False"), ("CEGUI:with_sdl", "True")]  # Build with SDL since OIS doesn't work on macOS
+        extra_options += [("CEGUI:with_ois", "False"), ("CEGUI:with_sdl", "True")]  # Build with SDL since OIS doesn't work on macOS
     # Disable VS2010 because of missing DirectX stuff
     # Disable x86 Linux builds (OGRE x86 package does not work at the moment)
     builder.builds = [
-        [dict(settings.items() + extra_settings), options, env_vars, build_requires]
+        [settings, dict(options.items() + extra_options), env_vars, build_requires]
         for settings, options, env_vars, build_requires in builder.builds
         if not (settings["compiler"] == "Visual Studio" and settings["compiler.version"] == "10")
         and not (os_info.is_linux and settings["arch"] == "x86")
