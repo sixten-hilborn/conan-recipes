0bbd4e0b212abac05fad47ba74ae657f38cea71e
Sixten Hilborn <hilborn90@gmail.com>
2017-07-20 01:06:52 +0200

    Add stuff

---
+++ conanfile.py
@@ -0,0 +1,59 @@
+from conans import ConanFile, ConfigureEnvironment, CMake
+from conans.tools import download, unzip
+import os
+import shutil
+
+class SmpegConan(ConanFile):
+    name = "smpeg"
+    version = "2.0.0"
+    description = "smpeg is an mpeg decoding library, which runs on just about any platform"
+    folder = "smpeg2-%s" % version
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False]
+    }
+    default_options = (
+        'shared=False',
+        'fPIC=True'
+    )
+    generators = "cmake"
+    requires = "SDL2/2.0.5@lasote/ci"
+    exports = ["CMakeLists.txt"]
+    url = "https://github.com/sixten-hilborn/conan-smpeg"
+    license = "Library GPL 2.0 - https://www.gnu.org/licenses/old-licenses/lgpl-2.0.html"
+
+    def config(self):
+        del self.settings.compiler.libcxx
+
+    def source(self):
+        zip_name = "%s.tar.gz" % self.folder
+        download("https://www.libsdl.org/projects/smpeg/release/%s" % zip_name, zip_name)
+        unzip(zip_name)
+        os.unlink(zip_name)
+        shutil.move("CMakeLists.txt", "%s/CMakeLists.txt" % self.folder)
+
+    def build(self):
+        cmake = CMake(self)
+        defs = {
+            'CMAKE_INSTALL_PREFIX': os.path.join(self.conanfile_directory, 'install'),
+            'CMAKE_POSITION_INDEPENDENT_CODE': self.options.fPIC
+        }
+        src = os.path.join(self.conanfile_directory, self.folder)
+        cmake.configure(build_dir='build', source_dir=src, defs=defs)
+        cmake.build(target='install')
+
+    def package(self):
+        """ Define your conan structure: headers, libs and data. After building your
+            project, this method is called to create a defined structure:
+        """
+        folder = 'install'
+        self.copy(pattern="*.h", dst="include", src=folder, keep_path=False)
+        self.copy(pattern="*.lib", dst="lib", src=folder, keep_path=False)
+        self.copy(pattern="*.dll*", dst="bin", src=folder, keep_path=False)
+        self.copy(pattern="*.a", dst="lib", src=folder, keep_path=False)
+        self.copy(pattern="*.so*", dst="lib", src=folder, keep_path=False)
+        self.copy(pattern="*.dylib*", dst="lib", src=folder, keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["smpeg2"]
---
+++ test_package/conanfile.py
@@ -0,0 +1,30 @@
+from conans import ConanFile, CMake
+import os
+
+############### CONFIGURE THESE VALUES ##################
+default_user = "hilborn"
+default_channel = "stable"
+#########################################################
+
+channel = os.getenv("CONAN_CHANNEL", default_channel)
+username = os.getenv("CONAN_USERNAME", default_user)
+
+
+class DefaultNameConan(ConanFile):
+    name = "DefaultName"
+    version = "0.1"
+    settings = "os", "compiler", "arch", "build_type"
+    generators = "cmake"
+    requires = "smpeg/2.0.0@%s/%s" % (username, channel)
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(build_dir='.')
+        cmake.build()
+
+    def imports(self):
+        self.copy(pattern="*.dll", dst="bin", src="bin")
+        self.copy(pattern="*.dylib", dst="bin", src="lib")
+
+    def test(self):
+        self.run("cd bin && .%sexample" % (os.sep))
