46ea15574613ddfaa191ac50286df856f90ef363
Sixten Hilborn <hilborn90@gmail.com>
2017-12-04 08:31:36 +0100

    Fix test_package/conanfile.py

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,7 +1,6 @@
 from conans import ConanFile, CMake
 import os
 
-
 class DefaultNameConan(ConanFile):
     name = "DefaultName"
     version = "0.1"
@@ -10,7 +9,7 @@ class DefaultNameConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(build_dir='.')
+        cmake.configure(source_dir=self.conanfile_directory, build_dir='.')
         cmake.build()
 
     def imports(self):
