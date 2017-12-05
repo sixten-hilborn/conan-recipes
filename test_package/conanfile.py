0df18354b53da03bdcdae95e87de8f0046897a60
Sixten Hilborn <hilborn90@gmail.com>
2017-12-05 20:10:34 +0100

    macOS RPATH

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,21 +1,11 @@
 from conans import ConanFile, CMake
 import os
 
-############### CONFIGURE THESE VALUES ##################
-default_user = "hilborn"
-default_channel = "stable"
-#########################################################
-
-channel = os.getenv("CONAN_CHANNEL", default_channel)
-username = os.getenv("CONAN_USERNAME", default_user)
-
-
 class DefaultNameConan(ConanFile):
     name = "DefaultName"
     version = "0.1"
     settings = "os", "compiler", "arch", "build_type"
     generators = "cmake"
-    requires = "smpeg/2.0.0@%s/%s" % (username, channel)
 
     def build(self):
         cmake = CMake(self)
