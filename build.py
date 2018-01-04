c56b596cd0151a6b7208cbee68b3c48a70d6c684
Sixten Hilborn <hilborn90@gmail.com>
2018-01-04 08:00:19 +0100

    Align to bincrafters template

--- build.py
+++ build.py
@@ -1,14 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from conan.packager import ConanMultiPackager
-from conans.tools import os_info
+from conans import tools
+import importlib
+import os
 
+
+def get_module_location():
+    repo = os.getenv("CONAN_MODULE_REPO", "https://raw.githubusercontent.com/bincrafters/conan-templates")
+    branch = os.getenv("CONAN_MODULE_BRANCH", "package_tools_modules")
+    return repo + "/" + branch
+
+    
+def get_module_name():
+    return os.getenv("CONAN_MODULE_NAME", "build_template_default")
+
+    
+def get_module_filename():
+    return get_module_name() + ".py"
+    
+    
+def get_module_url():
+    return get_module_location() + "/" + get_module_filename()
+
+    
 if __name__ == "__main__":
-    builder = ConanMultiPackager(args='--build=missing')
-    builder.add_common_builds(shared_option_name="SDL2_mixer:shared", pure_c=True)
-
-    filtered_builds = []
-    for settings, options, env_vars, build_requires in builder.builds:
-        if settings["arch"] != "x86":
-            filtered_builds.append([settings, options, env_vars, build_requires])
-    builder.builds = filtered_builds
-
+    
+    tools.download(get_module_url(), get_module_filename(), overwrite=True)
+    
+    module = importlib.import_module(get_module_name())
+    
+    builder = module.get_builder()
+    builder.add_remote_safe('bincrafters', 'https://api.bintray.com/conan/bincrafters/public-conan')
+    builder.add_remote_safe('dimi309', 'https://api.bintray.com/conan/dimi309/conan-packages')
     builder.run()
+
+    
--- conanfile.py
+++ conanfile.py
@@ -1,13 +1,27 @@
-from conans import ConanFile, CMake
-from conans.tools import download, unzip, replace_in_file
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from conans import ConanFile, CMake, tools
 import os
-import shutil
+
 
 class Sdl2MixerConan(ConanFile):
-    name = "SDL2_mixer"
+    name = "sdl2_mixer"
     version = "2.0.1"
-    description = "SDL2_mixer is a sample multi-channel audio mixer library."
-    folder = "SDL2_mixer-%s" % version
+    url = "https://github.com/sixten-hilborn/conan-sdl2_mixer"
+    description = "A sample multi-channel audio mixer library."
+
+    # Indicates License type of the packaged library
+    license = "zlib License - https://opensource.org/licenses/Zlib"
+
+    # Packages the license for the conanfile.py
+    exports = ["LICENSE.md"]
+
+    # Remove following lines if the target lib does not use cmake.
+    exports_sources = ["CMakeLists.txt"]
+    generators = "cmake"
+
+    # Options may need to change depending on the packaged library.
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
@@ -29,112 +43,72 @@ class Sdl2MixerConan(ConanFile):
         'with_libmodplug=False',
         'with_libmad=False'
     )
-    generators = "cmake"
+
+    # Custom attributes for Bincrafters recipe conventions
+    source_subfolder = "source_subfolder"
+    build_subfolder = "build_subfolder"
+
+    # Use version ranges for dependencies unless there's a reason not to
     requires = "sdl2/[>=2.0.6]@bincrafters/stable"
-    exports = ["CMakeLists.txt"]
-    url = "https://github.com/sixten-hilborn/conan-sdl2_mixer"
-    license = "zlib License - https://opensource.org/licenses/Zlib"
+
 
     def config(self):
         del self.settings.compiler.libcxx
 
+
     def requirements(self):
         if self.options.with_smpeg:
-            self.requires("smpeg/2.0.0@hilborn/stable")
+            self.requires("smpeg/[>=2.0.0]@hilborn/stable")
         if self.options.with_flac:
             self.requires("flac/[>=1.3.2]@bincrafters/stable")
         if self.options.with_ogg:
             self.requires("ogg/[>=1.3.3]@bincrafters/stable")
             self.requires("vorbis/[>=1.3.5]@bincrafters/stable")
         if self.options.with_libmikmod:
-            self.requires("libmikmod/3.3.11.1@hilborn/stable")
+            self.requires("libmikmod/[>=3.3.11.1]@hilborn/stable")
         if self.options.with_libmodplug:
-            self.requires("libmodplug/0.8.8.5@hilborn/stable")
+            self.requires("libmodplug/[>=0.8.8.5]@hilborn/stable")
         if self.options.with_libmad:
-            self.requires("libmad/0.15.1@hilborn/stable")
+            self.requires("libmad/[>=0.15.1]@hilborn/stable")
+
 
     def source(self):
-        zip_name = "%s.tar.gz" % self.folder
-        download("https://www.libsdl.org/projects/SDL_mixer/release/%s" % zip_name, zip_name)
-        unzip(zip_name)
-        os.unlink(zip_name)
-        shutil.move("CMakeLists.txt", "%s/CMakeLists.txt" % self.folder)
+        extracted_dir = "SDL2_mixer-" + self.version
+        tools.get("https://www.libsdl.org/projects/SDL_mixer/release/{0}.tar.gz".format(zip_name))
+
+        #Rename to "source_subfolder" is a convention to simplify later steps
+        os.rename(extracted_dir, self.source_subfolder)
+        shutil.move("CMakeLists.txt", "{0}/CMakeLists.txt".format(self.source_subfolder))
+
 
     def build(self):
         cmake = CMake(self)
-        defs = {
-            'CMAKE_INSTALL_PREFIX': os.path.join(self.conanfile_directory, 'install'),
-            'CMAKE_POSITION_INDEPENDENT_CODE': self.options.fPIC,
-            'SDLMIXER_SUPPORT_OGG_MUSIC': self.options.with_ogg,
-            'SDLMIXER_SUPPORT_MP3_MUSIC': self.options.with_smpeg,
-            'SDLMIXER_SUPPORT_MP3_MAD_MUSIC': self.options.with_libmad,
-            'SDLMIXER_SUPPORT_FLAC_MUSIC': self.options.with_flac,
-            'SDLMIXER_SUPPORT_MODPLUG_MUSIC': self.options.with_libmodplug,
-            'SDLMIXER_SUPPORT_MOD_MUSIC': self.options.with_libmikmod,
-            'SDLMIXER_SUPPORT_MID_MUSIC_FLUIDSYNTH': False
-        }
-        src = os.path.join(self.conanfile_directory, self.folder)
-        cmake.configure(build_dir='build', source_dir=src, defs=defs)
-        cmake.build(target='install')
-
-    # Deprecated for CMake
-    def build_copying_libs(self):
-        zip_name = "SDL2_mixer-devel-%s-VC.zip" % self.version
-        download("https://www.libsdl.org/projects/SDL_mixer/release/%s" % zip_name, zip_name)
-        unzip(zip_name)
-
-    # Deprecated for CMake
-    def build_with_make(self):
-        
-        env = ConfigureEnvironment(self.deps_cpp_info, self.settings)
-        if self.options.fPIC:
-            env_line = env.command_line.replace('CFLAGS="', 'CFLAGS="-fPIC ')
-        else:
-            env_line = env.command_line
-        env_line = env.command_line.replace('LDFLAGS="', 'LDFLAGS="-Wl,--no-as-needed ')
-            
-        custom_vars = '' # Original: 'LIBPNG_LIBS= SDL_LIBS= LIBPNG_CFLAGS=' TODO SDL_VERSION
-        sdl2_config_path = os.path.join(self.deps_cpp_info["SDL2"].lib_paths[0], "sdl2-config")
-         
-        self.run("cd %s" % self.folder)
-        self.run("chmod a+x %s/configure" % self.folder)
-        self.run("chmod a+x %s" % sdl2_config_path)
-        
-        configure_args = ["--disable-music-cmd", "--disable-music-mod-modplug", "--disable-music-mod-mikmod", "--disable-music-midi", # Stuff we give a shit about
-                          "--disable-music-ogg", "--disable-music-flac", "--disable-music-mp3", # Stuff we declare broken atm
-                          "--disable-sdltest", "--disable-smpegtest", ""] # We disable all manual tests
-        self.output.warn(env_line)
-        if self.settings.os == "Macos": # Fix rpath, we want empty rpaths, just pointing to lib file
-            old_str = "-install_name \\$rpath/"
-            new_str = "-install_name "
-            replace_in_file("%s/configure" % self.folder, old_str, new_str)
-        
-        configure_command = 'cd %s && %s SDL2_CONFIG=%s %s ./configure %s' % (self.folder, env_line, sdl2_config_path, custom_vars, " ".join(configure_args))
-        self.output.warn("Configure with: %s" % configure_command)
-        self.run(configure_command)
-
-        old_str = '\n# Commented by conan: CFLAGS ='
-        fpic = "-fPIC"  if self.options.fPIC else ""
-        m32 = "-m32" if self.settings.arch == "x86" else ""
-        debug = "-g" if self.settings.build_type == "Debug" else "-s -DNDEBUG"
-        new_str = '\nCFLAGS =%s %s %s %s\n# Commented by conan: CFLAGS =' % (" ".join(self.deps_cpp_info.cflags), fpic, m32, debug)
-        replace_in_file("%s/Makefile" % self.folder, old_str, new_str)
-        
-        self.run("cd %s && %s make" % (self.folder, env_line))
+        cmake.definitions['CMAKE_POSITION_INDEPENDENT_CODE'] = self.options.fPIC
+        cmake.definitions['SDLMIXER_SUPPORT_OGG_MUSIC'] = self.options.with_ogg
+        cmake.definitions['SDLMIXER_SUPPORT_MP3_MUSIC'] = self.options.with_smpeg
+        cmake.definitions['SDLMIXER_SUPPORT_MP3_MAD_MUSIC'] = self.options.with_libmad
+        cmake.definitions['SDLMIXER_SUPPORT_FLAC_MUSIC'] = self.options.with_flac
+        cmake.definitions['SDLMIXER_SUPPORT_MODPLUG_MUSIC'] = self.options.with_libmodplug
+        cmake.definitions['SDLMIXER_SUPPORT_MOD_MUSIC'] = self.options.with_libmikmod
+        cmake.definitions['SDLMIXER_SUPPORT_MID_MUSIC_FLUIDSYNTH'] = False
+        cmake.configure(build_folder=self.build_subfolder, source_folder=self.source_subfolder)
+        cmake.build()
+        cmake.install()
 
 
     def package(self):
-        """ Define your conan structure: headers, libs and data. After building your
-            project, this method is called to create a defined structure:
-        """
-        folder = 'install'
-        self.copy(pattern="*SDL_mixer.h", dst="include/SDL2", src=folder, keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", src=folder, keep_path=False)
-        self.copy(pattern="*.dll*", dst="bin", src=folder, keep_path=False)
-        self.copy(pattern="*.a", dst="lib", src=folder, keep_path=False)
-        self.copy(pattern="*.so*", dst="lib", src=folder, keep_path=False)
-        self.copy(pattern="*.dylib*", dst="lib", src=folder, keep_path=False)
+        # If the CMakeLists.txt has a proper install method, the steps below may be redundant
+        # If so, you can replace all the steps below with the word "pass"
+        include_folder = os.path.join(self.source_subfolder, "include")
+        self.copy(pattern="LICENSE")
+        self.copy(pattern="*SDL_mixer.h", dst="include/SDL2", src=include_folder)
+        self.copy(pattern="*.dll", dst="bin", keep_path=False)
+        self.copy(pattern="*.lib", dst="lib", keep_path=False)
+        self.copy(pattern="*.a", dst="lib", keep_path=False)
+        self.copy(pattern="*.so*", dst="lib", keep_path=False)
+        self.copy(pattern="*.dylib", dst="lib", keep_path=False)
+
 
     def package_info(self):
-        self.cpp_info.libs = ["SDL2_mixer"]
+        self.cpp_info.libs = tools.collect_libs(self)
         self.cpp_info.includedirs += ["include/SDL2"]
