c51037aa7f8ecdcb4fd3199c831d18be57fe478f
Sixten Hilborn <you@example.com>
2018-01-23 22:26:43 +0100

    Disable shared ci builds

--- build.py
+++ build.py
@@ -7,5 +7,6 @@ from bincrafters import build_template_default
 if __name__ == "__main__":
 
     builder = build_template_default.get_builder()
-
+    # Only build static in CI for now
+    builder.items = [build for build in builder.items if build[1]['sdl_gpu:shared'] == False]
     builder.run()
