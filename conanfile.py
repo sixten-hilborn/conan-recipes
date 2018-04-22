08ca36362e537ced9661d95d27721d7471eaa1c3
Sixten Hilborn <you@example.com>
2018-04-22 16:21:12 +0200

    Fix requirements

--- conanfile.py
+++ conanfile.py
@@ -25,7 +25,7 @@ class AlutConan(ConanFile):
     default_options = "shared=True"
     exports = ["CMakeLists.txt", "patches*"]
     requires = (
-        "openal-soft/1.17.2@hilborn/stable"
+        "openal/[>=1.18.2]@bincrafters/stable"
     )
     url = "http://github.com/sixten-hilborn/conan-alut"
     license = "https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html"
