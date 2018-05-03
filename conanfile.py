90d26ba30cda0c6086d23e724c20eab49b430ded
Sixten Hilborn <sixten.hilborn@gmail.com>
2018-05-04 00:11:12 +0200

    Update OpenGL requirements

--- conanfile.py
+++ conanfile.py
@@ -69,9 +69,8 @@ class CeguiConan(ConanFile):
         else:
             self.requires("freeimage/[>=3.17.0]@sixten-hilborn/stable")
         if self.options.with_opengl or self.options.with_opengl3 or self.options.with_opengles:
-            #self.requires("glew/2.0.0@coding3d/stable")
-            self.requires("glew/1.13.0@coding3d/stable")
-            self.requires("glm/[>=0.9.8]@g-truc/stable")
+            self.requires("glew/[>=2.1.0]@bincrafters/stable")
+            self.requires("glm/0.9.8.5@g-truc/stable")
             self.requires("glfw/[>=3.2.1]@bincrafters/stable")
 
 
